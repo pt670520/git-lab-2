5881e4f97e346810b616b4c2ba2d912361bc1ff6
Pramita <pt670520@ohio.edu>
2021-05-11 20:22:50 -0400

    You can find my program in git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Pramita Tiwari>
+ *        Date: <11th May,2021>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+ cout << "Hello Git!!" << endl;
+     /*add code*/
+    return 0;
+}// main
